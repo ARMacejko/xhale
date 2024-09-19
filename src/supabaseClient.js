2f9831ed9db0f7c1aae3848017865595c01c2cc7
ARMacejko <J@Macejkoj.com>
2024-09-19 13:34:19 -0400

    hummanah

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace with your actual Supabase URL and API Key
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Anon Key:', supabaseAnonKey);
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Supabase URL or anon key is missing!');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
