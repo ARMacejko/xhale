7beb62d8070e49fa7828457558a3864bcaec8100
ARMacejko <J@Macejkoj.com>
2024-09-21 14:20:55 -0400

    json be goofin

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Supabase URL and anon key retrieved from environment variables
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 console.log('Supabase URL:', supabaseUrl);
 console.log('Supabase Anon Key:', supabaseAnonKey);
+console.log('Test Variable:', process.env.NEXT_PUBLIC_TEST_VARIABLE);
+console.log('All environment variables:', process.env);
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Supabase URL or anon key is missing!');
